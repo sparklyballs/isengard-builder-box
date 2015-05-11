5b382f1bfb96ab7036b55f2c27224b8d89e29331
sparklyballs <sparklyballs@gmail.com>
2015-05-11 15:28:54 +0100

    Update source.sh

--- src/scripts/source.sh
+++ src/scripts/source.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-cd /root/build-area
 
 ##### Fetch source ###########
-
+ if [ -d "/Source/xbmc" ]; then
+rm -rf /root/build-area/xbmc
+cd /Source/xbmc
+echo "updating local source with latest git pull"
+git pull
+cp -pr /Source/xbmc /root/build-area/
+else
+echo "no local source found, cloning xbmc repo, this will take some time"
+rm -rf /root/build-area/xbmc
+cd /Source
 git clone https://github.com/xbmc/xbmc.git
-
+cp -pr /Source/xbmc /root/build-area/
+fi
 
 ##############################
 
