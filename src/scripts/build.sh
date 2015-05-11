5dce9b6e69eafe0431ddf029e990054491e47392
sparklyballs <sparklyballs@gmail.com>
2015-05-11 15:28:01 +0100

    Update build.sh

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -1,8 +1,50 @@
 #!/bin/bash
 
 cd /root/build-area/xbmc
-git checkout master
+git checkout 15.0b1-Isengard
+
+cp /Patches/* .
+git apply headless.patch
 ./bootstrap
-./configure
+./configure \
+--enable-nfs \
+--enable-upnp \
+--enable-ssh \
+--enable-libbluray \
+--disable-debug \
+--disable-vdpau \
+--disable-vaapi \
+--disable-crystalhd \
+--disable-vdadecoder \
+--disable-vtbdecoder \
+--disable-openmax \
+--disable-joystick \
+--disable-rsxs \
+--disable-projectm \
+--disable-rtmp \
+--disable-airplay \
+--disable-airtunes \
+--disable-dvdcss \
+--disable-optical-drive \
+--disable-libusb \
+--disable-libcec \
+--disable-libmp3lame \
+--disable-libcap \
+--disable-udev \
+--disable-libvorbisenc \
+--disable-asap-codec \
+--disable-afpclient \
+--disable-goom \
+--disable-fishbmc \
+--disable-spectrum \
+--disable-waveform \
+--disable-avahi \
+--disable-non-free \
+--disable-texturepacker \
+--disable-pulse \
+--disable-dbus \
+--disable-alsa \
+--disable-hal \
+--prefix=/opt/kodi-server
 make
 make DESTDIR=/root/project install
